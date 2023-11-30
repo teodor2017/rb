8f52b89b73f0075def94e0c018086b809fcd3a5d
teodor2017 <83453254+teodor2017@users.noreply.github.com>
2023-12-01 00:29:31 +0200

    Delete approvalWorkflow.ts

--- approvalWorkflow.ts
+++
@@ -1,7 +0,0 @@
-import { ReleaseRequest } from "../release/types";
-import { GateResponse } from "./gate";
-
-const enforceApprovalWorkflow = async (payload, octokit, request: ReleaseRequest):Promise<GateResponse> => null
-
-
-export default enforceApprovalWorkflow
