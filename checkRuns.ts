4964d8775dde7f8d649944cd9501068c0a02e822
teodor2017 <83453254+teodor2017@users.noreply.github.com>
2023-12-01 00:29:46 +0200

    Delete checkRuns.ts

--- checkRuns.ts
+++
@@ -1,6 +0,0 @@
-import { ReleaseRequest } from "@/release/types";
-import { GateResponse } from "./gate";
-
-const checkRuns = async (payload, octokit, request: ReleaseRequest):Promise<GateResponse> => null
-
-export default checkRuns
